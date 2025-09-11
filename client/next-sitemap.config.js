0fbd79b24687011d7b0c1ffdb3e17606db5cae39
its_sassinzz13 <66528388+sassinzz13@users.noreply.github.com>
2025-09-11 10:55:04 +0800

    Create next-sitemap.config.js

---
+++ client/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://stroberi-writes.vercel.app', // 🔗 your real domain
+  generateRobotsTxt: true,                      // ✅ also makes robots.txt
+  sitemapSize: 7000,                            // optional (for big sites)
+};
